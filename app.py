79a43013a4ebb6f96800bdff335f6fac823ae09a
TharunSom <tharunns2006@gmail.com>
2026-04-20 03:47:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import streamlit as st
-st.title("BharatUBID - AI Business Identify System")
-st.write(This system will identify duplicate business and generate UBID")
+import pandas as pd
+from src.entity_matching import match_entities
+from src.ubid_generator import generate_ubid
+
+st.title("🇮🇳 BharatUBID - AI Business Identity System")
+
+# Load dataset
+df = pd.read_csv("data/raw_data.csv")
+
+st.subheader("📂 Input Data")
+st.write(df)
+
+if st.button("Generate UBID"):
+    groups = match_entities(df)
+    ubid_map = generate_ubid(groups)
+
+    df['UBID'] = df.index.map(ubid_map)
+
+    st.subheader("✅ Processed Data with UBID")
+    st.write(df)
+
+    st.subheader("📊 Total Unique Businesses")
+    st.write(df['UBID'].nunique()
